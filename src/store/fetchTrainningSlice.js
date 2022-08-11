8cfc7f8a3e505bd7d2cbf47b4141fd499b8f108d
Free-devloper <92916471+Free-devleoper@users.noreply.github.com>
2022-08-12 04:30:58 +0500

    Update fetchTrainningSlice.js

--- src/store/fetchTrainningSlice.js
+++ src/store/fetchTrainningSlice.js
@@ -51,7 +51,7 @@ const fetch_trainningSlice=createSlice({
             state.trainning_data.val_loss=action.payload.train_data.val_loss;
             state.prediction_data=action.payload.prediction_data;
             state.tomorrow_data=action.payload.tommorow_price;
-            console.log(action)
+            // console.log(action)
           //  state.trainning_data_uid=action.payload.file_name;
             state.loading=false;
         },
